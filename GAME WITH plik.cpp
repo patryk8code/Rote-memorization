7f6846acfa19b00c0dd56db3b904daf1a849f3c8
patryk8code <patrykcode@gmail.com>
2025-05-23 15:47:26 +0200

    upload files
    Add project into github :)

---
+++ GAME WITH plik.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+#include <windows.h>
+#include <conio.h>
+using namespace std;
+    int i=0;
+    int z;
+    string a;
+    fstream tlum;
+//GRAAA
+    void zapis();
+    void odczyt();
+    void gra();
+    void wyjscie();
+//GRAAA
+int main(){
+    while(true){
+        cout<<"1:Dodaj do listy sˆowek..\n2:Poka¾ list©\n3:Zacznij gr©\n4:Wyjscie z programu\n>";
+        z=getch();
+        switch(z){
+            case '1':zapis();     break;
+            case '2':odczyt();    break;
+            case '3':gra();       break;
+            case '4':wyjscie();   break;
+        };
+    }
+return(0);
+}
+
+void zapis(){
+    cout<<"Witaj w swiecie tˆumaczenia!";
+    cout<<"\n Wpisz sˆowo po Polsku a nast©pnie tˆumaczenie\n>";
+
+
+    tlum.open("dane.txt",ios::out|ios::app);
+
+    while(true){
+    if(i>0){
+        system("cls");
+        cout<<"Wpisz 'c' aby zakonczyc\n";
+        cout<<"Podaj po Polsku\n> ";
+        }
+    getline(cin,a);
+        if(a=="c"){break;}
+    tlum<<a<<endl;
+    cout<<"Teraz podaj tˆumaczenie\n> ";
+    getline(cin,a);
+    tlum<<a<<endl;
+    i++;
+    }
+    tlum.close();
+    Sleep(250);
+
+
+
+    }
+void odczyt(){
+    system("cls");
+    tlum.open("dane.txt",ios::in);
+    if(tlum.good()==false){
+        cout<<"Nie ma takiego pliku, najpierw zapisz dane ";
+        exit(0);}
+    int nr=1;
+    string P,A;
+    while(getline(tlum,a)){
+        int igla;
+        igla++;
+        cout<<igla<<": "<<a<<endl;
+        switch(nr){
+            case 1:P=a;    break;
+            case 2:A=a;    break;
+            }
+         nr++;
+         if (nr==3)nr=1;
+    }
+    tlum.close();
+    cout<<"\n~~~~~~~~~~~~~~~~~~~~\n\n";
+}
+void gra(){
+    tlum.open("dane.txt",ios::in);
+    if(tlum.good()==false){
+        cout<<"Nie ma takiego pliku, najpierw zapisz dane ";
+        exit(0);}
+    int nr=1;
+    string P,A;
+    string odpowiedz;
+    while(getline(tlum,a)){
+        switch(nr){
+            case 1:P=a;    break;
+            case 2:A=a;    break;
+            }
+         nr++;
+         if (nr==3)nr=1;
+         if (nr==1){
+         cout<<nr<<" Po Polsku: "<<P<<"\nPo NIEMIECKU: ";
+         cin>>odpowiedz;
+            if(odpowiedz==A){
+                cout<<"JESTE— SUPER!\n";
+            } else {cout<<"Pudlo, odpowied« to: "<<A<<endl;}
+         }
+    }
+    tlum.close();
+    cout<<"\nTo koniec, tym razem dodaj ich wiecej!\n\a";
+}
+void wyjscie(){
+exit(0);
+    }
